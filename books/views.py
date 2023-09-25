0d2d150aee648d8f65285ee981397ac0b98437b9
MagerOK <farmarker@gmail.com>
2023-09-25 11:19:40 +0200

    feature: added html book information view

--- books/views.py
+++ books/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from books.models import Book
+
+def get_book_details_html(request, book_id):
+    book = get_object_or_404(Book, pk=book_id)
+    context = {'book': book}
+    return render(request, 'book_details.html', context)
