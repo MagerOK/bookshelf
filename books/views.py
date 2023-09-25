40ac2bba6235b03bd8b756325037eee644394464
MagerOK <farmarker@gmail.com>
2023-09-25 17:12:30 +0200

    feature: added view that shows book info

--- books/views.py
+++ books/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+from books.models import Book
+from books.serializer import serialize_book
+
+
+
+
+def get_book_json_view(request, book_id: int):
+    book = Book.objects.filter(pk=book_id).first()
+    if not book:
+        return JsonResponse(data={'data': {}, "error": "Book not found"}, status=404)
+    serialized_book = serialize_book(book)
+    return JsonResponse(data={'data': serialized_book})
--- bookshelf/urls.py
+++ bookshelf/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 
+from books.views import get_book_json_view
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/books/<int:book_id>/', get_book_json_view)
 ]
