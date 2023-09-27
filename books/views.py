e9698886da368f80b57d11caf4d7b460ff382aa0
MagerOK <farmarker@gmail.com>
2023-09-27 17:07:32 +0200

    fix: added very few logic to html and added type annotations

--- books/views.py
+++ books/views.py
@@ -1,9 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 from books.models import Book
 
 
-def get_all_books_html_view(request):
+def get_all_books_html_view(request: HttpResponse) -> HttpResponse:
     books = Book.objects.all()
     context = {'books': books}
     return render(request, 'books_table.html', context)
