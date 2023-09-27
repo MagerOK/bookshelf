12f3e70875c76727e2f0a0c7faba695520ff8f8d
MagerOK <farmarker@gmail.com>
2023-09-27 17:19:03 +0200

    added type annotations

--- books/views.py
+++ books/views.py
@@ -1,8 +1,9 @@
+from django.http import Http404, HttpResponse
 from django.shortcuts import render, get_object_or_404
 
 from books.models import Book
 
-def get_book_details_html(request, book_id):
+def get_book_details_html(request: HttpResponse, book_id: int) -> HttpResponse:
     book = get_object_or_404(Book, pk=book_id)
     context = {'book': book}
     return render(request, 'book_details.html', context)
